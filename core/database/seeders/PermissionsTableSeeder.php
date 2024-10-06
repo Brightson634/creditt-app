e0744df31e428dfde1dff270fddea210367d538e
Kibooli-Felix <kiboolif@gmail.com>
2024-10-06 23:12:20 +0300

    Update on PermissionsTable

--- core/database/seeders/PermissionsTableSeeder.php
+++ core/database/seeders/PermissionsTableSeeder.php
@@ -29,6 +29,7 @@ class PermissionsTableSeeder extends Seeder
             ['name' => 'add_loan_product'],
             ['name' => 'edit_loan_product'],
             ['name' => 'delete_loan_product'],
+            ['name' => 'view_loan_products'],
             ['name' => 'add_loan_repayment'],
             ['name' => 'edit_loan_repayment'],
             ['name' => 'delete_loan_repayment'],
