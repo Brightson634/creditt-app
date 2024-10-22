23bb0fddc38686fd35899151668ea8130359ec0c
Kibooli-Felix <kiboolif@gmail.com>
2024-10-22 21:57:27 +0300

    Add new columns to settings table numb_of_approving_authorities and numb_of_reviewing_authorities

---
+++ core/database/migrations/2024_10_22_204831_add_approving_and_reviewing_authorities_to_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApprovingAndReviewingAuthoritiesToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+            $table->integer('numb_of_approving_authorities')->default(0);
+            $table->integer('numb_of_reviewing_authorities')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+            $table->dropColumn('numb_of_approving_authorities');
+            $table->dropColumn('numb_of_reviewing_authorities');
+        });
+    }
+}
