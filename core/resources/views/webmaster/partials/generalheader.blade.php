1cf8bfa12a9476bc74901841b6585ffb7dcdf22a
Kibooli-Felix <kiboolif@gmail.com>
2024-12-03 22:44:15 +0300

    Limit payments button

--- core/resources/views/webmaster/partials/generalheader.blade.php
+++ core/resources/views/webmaster/partials/generalheader.blade.php
@@ -26,21 +26,45 @@
                 <h6>All Categories</h6> --}}
             </div><!-- media-body -->
         </div><!-- media -->
+        @can('add_loan_repayment')
         <a href="{{ route('webmaster.loanpayment.create') }}" class="btn btn-purple">Make Payments</a>
+        @endcan
     </div>
 </div><!-- az-dashboard-one-title -->
 
 <div class="az-dashboard-nav">
-  <nav class="nav">
-  @can('view_main_dashboard')
-    <a class="nav-link active"  href="{{route('webmaster.dashboard')}}">Dashboard</a>
-  @endcan
-    <a class="nav-link active"  href="{{route('webmaster.calendar.view')}}">Calendar</a>
-    <a class="nav-link" href="{{route('webmaster.overview')}}">Accounting</a>
-    <a class="nav-link"  href="{{ route('webmaster.loan.create') }}">Loan Application</a>
-    <a class="nav-link"  href="{{ route('webmaster.member.create') }}">Members</a>
-    <a class="nav-link"  href="{{ route('webmaster.investment.create') }}">Investments</a>
-    <a class="nav-link"  href="{{ route('webmaster.saving.create') }}">Savings</a>
-    <a class="nav-link" data-toggle="tab" href="#">More</a>
-  </nav>
-</div>
+    <nav class="nav">
+      @can('view_main_dashboard')
+        <a class="nav-link {{ request()->routeIs('webmaster.dashboard') ? 'active' : '' }}"  
+           href="{{ route('webmaster.dashboard') }}">
+          Dashboard
+        </a>
+      @endcan
+      <a class="nav-link {{ request()->routeIs('webmaster.calendar.view') ? 'active' : '' }}" 
+         href="{{ route('webmaster.calendar.view') }}">
+        Calendar
+      </a>
+      <a class="nav-link {{ request()->routeIs('webmaster.overview') ? 'active' : '' }}" 
+         href="{{ route('webmaster.overview') }}">
+        Accounting
+      </a>
+      <a class="nav-link {{ request()->routeIs('webmaster.loan.create') ? 'active' : '' }}" 
+         href="{{ route('webmaster.loan.create') }}">
+        Loan Application
+      </a>
+      <a class="nav-link {{ request()->routeIs('webmaster.member.create') ? 'active' : '' }}" 
+         href="{{ route('webmaster.member.create') }}">
+        Members
+      </a>
+      <a class="nav-link {{ request()->routeIs('webmaster.investment.create') ? 'active' : '' }}" 
+         href="{{ route('webmaster.investment.create') }}">
+        Investments
+      </a>
+      <a class="nav-link {{ request()->routeIs('webmaster.saving.create') ? 'active' : '' }}" 
+         href="{{ route('webmaster.saving.create') }}">
+        Savings
+      </a>
+      <a class="nav-link" data-toggle="tab" href="#">More</a>
+    </nav>
+  </div>
+  
